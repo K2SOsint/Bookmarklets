b5aab09dc242ec44997b16d5a4b99c5722359322
K2SOsint <85451432+K2SOsint@users.noreply.github.com>
2022-07-16 18:36:58 +0200

    Update AWCensys.js

--- AWCensys.js
+++ AWCensys.js
@@ -1,5 +1,5 @@
 javascript:
 var input = prompt("Please enter AW code without AW-");
-var censysua = "https://search.censys.io/search?resource=hosts&sort=RELEVANCE&per_page=25&virtual_hosts=EXCLUDE&q=services.http.response.body%3A" + '"' + "%27_setAccount%27" + '"' + "+and+services.http.response.body%3A" + '"' + "aw-" + input + '"';
+var censysua = "https://search.censys.io/search?resource=hosts&sort=RELEVANCE&per_page=25&virtual_hosts=EXCLUDE&q=services.http.response.body%3A" + '"' + "%27gtag%27" + '"' + "+and+services.http.response.body%3A" + '"' + "aw-" + input + '"';
 function CensysAW() { window.open(censysua, "_blank"); }
 CensysAW();
