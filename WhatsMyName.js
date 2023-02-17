0eb114fb15a030d16fbfd7339fcc67185c2068e4
K2SOsint <85451432+K2SOsint@users.noreply.github.com>
2023-02-17 08:33:38 +0100

    Create WhatsMyName.js

---
+++ WhatsMyName.js
@@ -0,0 +1,12 @@
+javascript:
+
+var input = prompt("Enter username to search on WhatsMyName");
+var wmn = ("https://whatsmyname.app/?q=") + input;
+
+function Wmn() {
+	window.open(wmn);
+}
+
+Wmn()
+
+/*Kudos to @webbreacher and everyone involved in the WhatsMyName project */
