f0e2acf99c42784100c35f73ac0b5b7119465cc3
K2SOsint <85451432+K2SOsint@users.noreply.github.com>
2023-07-23 14:18:38 +0200

    Create Censys_GTM.js

---
+++ Censys_GTM.js
@@ -0,0 +1,5 @@
+javascript:
+var input = prompt("Enter the part of the GTM code, that starts after GTM-");
+var censgtm = "https://search.censys.io/search?resource=hosts&sort=RELEVANCE&per_page=25&virtual_hosts=EXCLUDE&q=services.http.response.body%3A" + '"' + "gtm-" + input + '"';
+function CenSys() { window.open(censgtm, "_blank"); } 
+CenSys();
