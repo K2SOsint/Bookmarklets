52f7377bc924dc7caa1d720603032f6928601a22
K2SOsint <85451432+K2SOsint@users.noreply.github.com>
2023-07-23 14:18:58 +0200

    Delete AWCensys.js

--- AWCensys.js
+++
@@ -1,5 +0,0 @@
-javascript:
-var input = prompt("Please enter AW code without AW-");
-var censysaw = "https://search.censys.io/search?resource=hosts&sort=RELEVANCE&per_page=25&virtual_hosts=EXCLUDE&q=services.http.response.body%3A" + '"' + "%27gtag%27" + '"' + "+and+services.http.response.body%3A" + '"' + "aw-" + input + '"';
-function CensysAW() { window.open(censysaw, "_blank"); }
-CensysAW();
