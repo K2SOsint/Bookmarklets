f833202d0261b575b55fd9bae47a2dc42368cead
K2SOsint <85451432+K2SOsint@users.noreply.github.com>
2023-07-23 14:19:43 +0200

    Delete UACensys.js

--- UACensys.js
+++
@@ -1,5 +0,0 @@
-javascript:
-var input = prompt("Please enter UA code without UA-");
-var censysua = "https://search.censys.io/search?resource=hosts&sort=RELEVANCE&per_page=25&virtual_hosts=EXCLUDE&q=services.http.response.body%3A" + '"' + "%27_setAccount%27" + '"' + "+and+services.http.response.body%3A" + '"' + "ua-" + input + '"';
-function CensysUA() { window.open(censysua, "_blank"); }
-CensysUA();
