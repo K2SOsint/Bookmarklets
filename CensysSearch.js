3bfa436159c345fbca53720272974a640434795a
K2SOsint <85451432+K2SOsint@users.noreply.github.com>
2023-12-27 15:10:06 +0100

    Delete CensysSearch.js

--- CensysSearch.js
+++
@@ -1,5 +0,0 @@
-javascript:
-var input = prompt("Enter text to search for in collected info from http.response body (datalayer):");
-var censsearch = "https://search.censys.io/search?resource=hosts&sort=RELEVANCE&per_page=25&virtual_hosts=EXCLUDE&q=services.http.response.body%3A" + '"' + input + '"' + "+and+services.http.response.body%3A" + '"' + "dataLayer" + '"';
-function CenSys() { window.open(censsearch, "_blank"); } 
-CenSys();
