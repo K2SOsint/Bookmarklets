dafe67feb76533117a28bae3d5f757dd4f1c2d32
Henri <85451432+K2SOsint@users.noreply.github.com>
2026-04-23 12:31:08 +0200

    Update and rename BuiltwithCensys.js to Builtwith.js

--- Builtwith.js
+++ Builtwith.js
@@ -2,7 +2,6 @@ javascript:
 
 var input = prompt('Enter the analytics-code to search');
 var builtwith = 'https://builtwith.com/relationships/tag/' + input;
-var censys = 'https://search.censys.io/search?resource=hosts&sort=RELEVANCE&per_page=25&virtual_hosts=INCLUDE&q=services.http.response.body%3A' + input;
 
 function DoSomething() {
 	window.open(builtwith, '_blank');
